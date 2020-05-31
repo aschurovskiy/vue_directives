8127d55112d84bf27db1c5141f70dd62ccded47f
aschurovskiy <alexander.schurovskiy@protonmail.com>
2020-05-31 14:13:17 +0300

    initial

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.directive('highlight', {
+  bind: function(el, binding, vnode){
+    // el.style.backgroudColor='green';
+    // el.style.backgroundColor=binding.value;
+    var delay =0;
+    if (binding.modifiers['delayed']) {
+      delay=3000;
+    }
+
+    setTimeout(()=>{
+      if (binding.arg =='background'){
+        el.style.backgroundColor=binding.value;
+      } else {
+        el.style.color=binding.value;
+      }
+    }, delay)
+  }
+});
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
